634be8df8fab4f7e0a1044abf8a002ef47624f26
jiahe <jiahe634@gmail.com>
2024-03-11 20:53:54 +0800

    124. 二叉树中的最大路径和

---
+++ src/main/java/org/algorithm/difficulty/BestTimeBuySellStockIV.java
@@ -0,0 +1,39 @@
+package org.algorithm.difficulty;
+
+import java.util.Arrays;
+
+/**
+ * @author jiahe
+ * 188. 买卖股票的最佳时机 IV
+ */
+public class BestTimeBuySellStockIV {
+
+    public int maxProfit(int k, int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+
+        int n = prices.length;
+        k = Math.min(k, n / 2);
+        int[] buy = new int[k + 1];
+        int[] sell = new int[k + 1];
+
+        buy[0] = -prices[0];
+        sell[0] = 0;
+        for (int i = 1; i <= k; ++i) {
+            buy[i] = sell[i] = Integer.MIN_VALUE / 2;
+        }
+
+        for (int i = 1; i < n; ++i) {
+            buy[0] = Math.max(buy[0], sell[0] - prices[i]);
+            for (int j = 1; j <= k; ++j) {
+                buy[j] = Math.max(buy[j], sell[j] - prices[i]);
+                sell[j] = Math.max(sell[j], buy[j - 1] + prices[i]);
+            }
+        }
+
+        return Arrays.stream(sell).max().getAsInt();
+    }
+
+}
+
--- src/test/java/org/algorithm/difficulty/BasicCalculatorTest.java
+++ src/test/java/org/algorithm/difficulty/BasicCalculatorTest.java
@@ -2,8 +2,6 @@ package org.algorithm.difficulty;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class BasicCalculatorTest {
 
     @Test
