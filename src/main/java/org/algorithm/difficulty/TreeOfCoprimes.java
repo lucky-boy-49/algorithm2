25455374b25666ab6443e6006da38d42cb132e89
贺佳 <hejia1744709356@163.com>
2024-04-11 22:43:28 +0800

    1766. 互质树

--- src/main/java/org/algorithm/difficulty/TreeOfCoprimes.java
+++ src/main/java/org/algorithm/difficulty/TreeOfCoprimes.java
@@ -1,93 +1,88 @@
 package org.algorithm.difficulty;
 
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 /**
  * 1766.互质树
+ *
  * @author jiahe
  */
 public class TreeOfCoprimes {
 
-    private final List<Integer> parent = new LinkedList<>();
-    private final Map<String, Integer> map = new HashMap<>();
-    private final Map<Integer, List<Integer>> map2 = new HashMap<>();
+    private static final int MX = 51;
+    private static final int[][] COPRIME = new int[MX][MX];
+
+    static {
+        // 预处理
+        // coprime[i] 保存 [1, MX) 中与 i 互质的所有元素
+        for (int i = 1; i < MX; i++) {
+            int k = 0;
+            for (int j = 1; j < MX; j++) {
+                if (gcd(i, j) == 1) {
+                    COPRIME[i][k++] = j;
+                }
+            }
+        }
+    }
 
     public int[] getCoprimes(int[] nums, int[][] edges) {
-
-        for (int[] edge : edges) {
-            if (map2.containsKey(edge[0])) {
-                map2.get(edge[0]).add(edge[1]);
-            } else {
-                List<Integer> list = new LinkedList<>();
-                list.add(edge[1]);
-                map2.put(edge[0], list);
-            }
-            if (map2.containsKey(edge[1])) {
-                map2.get(edge[1]).add(edge[0]);
-            } else {
-                List<Integer> list = new LinkedList<>();
-                list.add(edge[0]);
-                map2.put(edge[1], list);
-            }
+        int n = nums.length;
+        List<Integer>[] g = new ArrayList[n];
+        Arrays.setAll(g, i -> new ArrayList<>());
+        for (int[] e : edges) {
+            int x = e[0];
+            int y = e[1];
+            g[x].add(y);
+            g[y].add(x);
         }
 
-        int[] ans = new int[nums.length];
-        ans[0] = -1;
-
-        parent.add(0);
-
-        if (map2.isEmpty()) {
-            return ans;
-        }
-
-        map2.get(0).forEach(n -> {
-            map.put(0 + ":" + n, 1);
-            map.put(n + ":" + 0, 1);
-            dfs(ans, nums, edges, n);
-            map.remove(0 + ":" + n);
-            map.remove(n + ":" + 0);
-        });
-
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        int[] valDepth = new int[MX];
+        int[] valNodeId = new int[MX];
+        dfs(0, -1, 1, g, nums, ans, valDepth, valNodeId);
         return ans;
     }
 
-    private void dfs(int[] ans, int[] nums, int[][] edges, int i) {
-        int an = -1;
-        for (Integer j : parent) {
-            if (gcd(nums[j], nums[i]) == 1) {
-                an = j;
+    private void dfs(int x, int fa, int depth, List<Integer>[] g, int[] nums, int[] ans, int[] valDepth, int[] valNodeId) {
+        // x 的节点值
+        int val = nums[x];
+
+        // 计算与 val 互质的祖先节点值中，节点深度最大的节点编号
+        int maxDepth = 0;
+        for (int j : COPRIME[val]) {
+            if (j == 0) {
+                break;
+            }
+            if (valDepth[j] > maxDepth) {
+                maxDepth = valDepth[j];
+                ans[x] = valNodeId[j];
             }
         }
-        ans[i] = an;
-        parent.add(i);
-        if (map2.containsKey(i)) {
-            map2.get(i).forEach(n -> {
-                if (!map.containsKey(i + ":" + n)) {
-                    map.put(i + ":" + n, 1);
-                    map.put(n + ":" + i, 1);
-                    dfs(ans, nums, edges, n);
-                    map.remove(i + ":" + n);
-                    map.remove(n + ":" + i);
-                }
-            });
+
+        // tmpDepth 和 tmpNodeId 用于恢复现场
+        int tmpDepth = valDepth[val];
+        int tmpNodeId = valNodeId[val];
+
+        // 保存 val 对应的节点深度和节点编号
+        valDepth[val] = depth;
+        valNodeId[val] = x;
+
+        // 向下递归
+        for (int y : g[x]) {
+            if (y != fa) {
+                dfs(y, x, depth + 1, g, nums, ans, valDepth, valNodeId);
+            }
         }
 
-        parent.remove(parent.size() - 1);
+        // 恢复现场
+        valDepth[val] = tmpDepth;
+        valNodeId[val] = tmpNodeId;
     }
 
-    private int gcd(int x, int y) {
-        int max = Math.max(x, y);
-        int min = Math.min(x, y);
-        int r = max % min;
-        while (r != 0) {
-            max = min;
-            min = r;
-            r = max % min;
-        }
-        return min;
+    private static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
-
 }
